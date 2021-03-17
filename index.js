945109d35b239d7f13fefe547e33f8049819166d
hotsnow199 <49254639+hotsnow199@users.noreply.github.com>
2021-03-17 20:45:24 +0700

    connect mongo

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 import express from 'express';
 import { config } from 'dotenv';
+import mongoose from 'mongoose';
+mongoose.connect(
+  'mongodb+srv://hoang:142536@shop-mnml.et8ke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 const app = express();
 config();
 app.use('/', (req, res) => {
