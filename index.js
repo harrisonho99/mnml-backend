5f4827479107a429840c7d37c5cf57c960ffb73f
valbenia <huynguyen.rido@gmail.com>
2021-03-19 16:10:47 +0000

    abc

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require("./models/ProductModel")
+const {Schema} = mongoose;
 // config variable environment
 require('dotenv').config();
 
 const app = express();
-app.use('/', (_, res) => {
-  res.send('<h1>hello world</h1>').end();
+app.get('/products', (req, res, next) => {
+// res.send("hello, " + req.params.id)
+Product.find().exec().then(listProducst =>{
+    res.json({list : listProducst})
+}).catch((err )=>{
+console.error(err)
+})
 });
+
+
 //connect mongodb
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
@@ -19,6 +28,11 @@ db.on('error', (err) => {
   console.error(err);
 });
 db.on('open', () => {
-  app.listen(4000, () => {});
-  console.log('listen on port 4000');
+  app.listen(4000, async() => {
+
+      console.log('listen on port 4000');
+        // const product1 = new Product ({name :"huy", count : 10, create_by: "hoang", })
+        // await product1.save()
+    });
+    
 });
---
+++ models/ProductModel.js
@@ -0,0 +1,17 @@
+const  mongoose = require("mongoose")
+const {Schema} = mongoose
+const productSchema = new Schema ({
+   name:{type:String, required:true},
+   price:{type: Number, required: true},
+   imageURL:{type:String, required: true},
+   listImageURL:[String],
+   color:[String],
+   mainColor:{type :String , required :true },
+   size:{type: String, required: true},
+   productType:{type: String, required: true}
+});
+productSchema.methods.getAllProducts = function (){
+    return this.find().exec()
+}
+const Product = mongoose.model("Product",productSchema);
+module.exports = Product
